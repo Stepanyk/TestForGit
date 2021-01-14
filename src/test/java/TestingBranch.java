4494e3d64983e6748347e006c1dc501aad965400
Stepanyk <andrejstepanuk16@gmail.com>
2021-01-14 16:45:31 +0200

    TestingNewBranch

--- src/test/java/TestingBranch.java
+++ src/test/java/TestingBranch.java
@@ -1,3 +1,24 @@
-public class TestingBranch {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
+public class TestingBranch {
+    WebDriver driver;
+
+    @BeforeTest
+    public void Before() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+
+    }
+
+    @Test
+    public void Test() {
+        driver.get("https://git-scm.com/book/ru/v2/%D0%9F%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5-C%3A-%D0%9A%D0%BE%D0%BC%D0%B0%D0%BD%D0%B4%D1%8B-Git-%D0%9E%D1%81%D0%BD%D0%BE%D0%B2%D0%BD%D1%8B%D0%B5-%D0%BA%D0%BE%D0%BC%D0%B0%D0%BD%D0%B4%D1%8B");
+
+
+    }
 }
